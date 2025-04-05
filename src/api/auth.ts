1ab444462d7f79a835aed69b5ba6767e8f8bc11c
dsbbl <domin25001@gmail.com>
2025-04-05 18:51:32 +0200

    Add prod redirect

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,23 +1,27 @@
 const SPOTIFY_CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-const REDIRECT_URI = import.meta.env.VITE_SPOTIFY_REDIRECT_URI;
+const REDIRECT_URI =
+  import.meta.env.MODE === 'production'
+    ? import.meta.env.VITE_SPOTIFY_REDIRECT_URI_PRODUCTION
+    : import.meta.env.VITE_SPOTIFY_REDIRECT_URI;
+
 const SCOPES = [
-    'user-read-private',
-    'user-read-email',
-    'playlist-read-private',
-    'playlist-modify-public',
-    'playlist-modify-private',
-    'user-library-read',
-    'user-top-read'
+  'user-read-private',
+  'user-read-email',
+  'playlist-read-private',
+  'playlist-modify-public',
+  'playlist-modify-private',
+  'user-library-read',
+  'user-top-read',
 ].join(' ');
 
 export const generateSpotifyLoginUrl = () => {
-    const params = new URLSearchParams({
-        client_id: SPOTIFY_CLIENT_ID,
-        response_type: 'token', 
-        redirect_uri: REDIRECT_URI,
-        scope: SCOPES,
-        show_dialog: 'true',
-    });
+  const params = new URLSearchParams({
+    client_id: SPOTIFY_CLIENT_ID,
+    response_type: 'token',
+    redirect_uri: REDIRECT_URI,
+    scope: SCOPES,
+    show_dialog: 'true',
+  });
 
-    return `https://accounts.spotify.com/authorize?${params.toString()}`;
+  return `https://accounts.spotify.com/authorize?${params.toString()}`;
 };
